1c895b5bfa43f0d3cf1450f6929990254015f44d
Fdrv <oldgot@gmail.com>
2021-05-17 18:11:09 +0300

    Add blank start page

--- main.py
+++
@@ -1,7 +0,0 @@
-users = [
-  {'username': 'Даша', 'posts': 25},
-  {'username': 'Валера', 'posts': 10},
-  {'username': 'Ибрагим', 'posts': 56},
-]
-
-print(min(users, key=lambda x: -x['username']))
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from where_to_go import views
 
 urlpatterns = [
+    path('', views.main_page),
     path('admin/', admin.site.urls),
 ]
