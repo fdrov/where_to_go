a3b1cae53a1b9ee90c9b195d450668a5f829f301
Fdrv <oldgot@gmail.com>
2021-05-26 13:28:00 +0300

    Add inline images to Places

--- places/admin.py
+++ places/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
     pass
-
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
