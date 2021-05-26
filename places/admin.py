efe68049af6075fbe590592b14e4aa7dfafd73fe
Fdrv <oldgot@gmail.com>
2021-05-27 00:01:44 +0300

    Make photos sortable by drag

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,4 @@
+from adminsortable2.admin import SortableInlineAdminMixin
 from django.contrib import admin
 from django.utils.html import format_html
 
@@ -12,9 +13,10 @@ def image_preview(obj):
     )
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     readonly_fields = [image_preview]
+    extra = 1
 
 
 @admin.register(Place)
