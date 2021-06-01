0b9fa34b69dc21295553b28866c7d4836341f763
Fdrv <oldgot@gmail.com>
2021-06-01 18:48:28 +0300

    Allow fields to be optional

--- places/models.py
+++ places/models.py
@@ -4,8 +4,10 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=300, verbose_name='Название')
-    description_short = models.TextField(verbose_name='Сокращенное описание')
-    description_long = HTMLField(verbose_name='Полное описание')
+    description_short = models.TextField(verbose_name='Сокращенное описание',
+                                         blank=True)
+    description_long = HTMLField(verbose_name='Полное описание',
+                                 blank=True)
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
 
@@ -21,8 +23,7 @@ class Image(models.Model):
     img = models.ImageField(verbose_name='Фото')
     position = models.PositiveSmallIntegerField(verbose_name='Позиция фото',
                                                 default=0,
-                                                blank=False,
-                                                null=False)
+                                                blank=True)
 
     class Meta(object):
         ordering = ['position']
